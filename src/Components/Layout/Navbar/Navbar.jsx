e656760749af2aa130d71e1638c6a10a2d8ae4bc
sohseyedi <sohseyedi@gmail.com>
2022-08-13 14:37:40 +0430

    complete ui

--- src/Components/Layout/Navbar/Navbar.jsx
+++ src/Components/Layout/Navbar/Navbar.jsx
@@ -1,8 +1,9 @@
-
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.scss";
 
-const Navbar = ({lang, setLang}) => {
+const Navbar = ({ lang, setLang }) => {
+  const [isActive, setIsActive] = useState(false);
 
   return (
     <nav className="navs" dir={lang ? "ltr" : "rtl"}>
@@ -12,11 +13,28 @@ const Navbar = ({lang, setLang}) => {
             <h2>Logo</h2>
             <div className="navs-container__links">
               <Link to={"/"} className={"navs-container__links-auth"}>
-                Account
+                User Account
               </Link>
-              <div className={"navs-container__links-lang"}>
-                <span onClick={() => setLang(false)}>persian</span>
-                <span onClick={() => setLang(true)}>english</span>
+              <div className="navs-container__links-lang">
+                <div
+                  className="navs-container__links-lang__btn"
+                  onClick={() => setIsActive(!isActive)}
+                >
+                  English
+                </div>
+                {isActive && (
+                  <div className="navs-container__links-lang__items">
+                    <div
+                      className="navs-container__links-lang__items-text"
+                      onClick={() => {
+                        setLang(false);
+                        setIsActive(false);
+                      }}
+                    >
+                      Persian
+                    </div>
+                  </div>
+                )}
               </div>
             </div>
           </>
@@ -24,13 +42,30 @@ const Navbar = ({lang, setLang}) => {
           <>
             <h2>لوگو</h2>
             <div className="navs-container__links">
+              <div className="navs-container__links-lang">
+                <div
+                  className="navs-container__links-lang__btn"
+                  onClick={() => setIsActive(!isActive)}
+                >
+                  فارسی
+                </div>
+                {isActive && (
+                  <div className="navs-container__links-lang__items">
+                    <div
+                      className="navs-container__links-lang__items-text"
+                      onClick={() => {
+                        setLang(true);
+                        setIsActive(false);
+                      }}
+                    >
+                      انگلیسی
+                    </div>
+                  </div>
+                )}
+              </div>
               <Link to={"/"} className="navs-container__links-auth">
                 حساب کاربری
               </Link>
-              <div className="navs-container__links-lang">
-                <span onClick={() => setLang(false)}>فارسی</span>
-                <span onClick={() => setLang(true)}>انگلیسی</span>
-              </div>
             </div>
           </>
         )}
